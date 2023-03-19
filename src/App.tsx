66e91930288cee75499671a0553c45f9437148db
Nick Hageman <nicholas-hageman@uiowa.edu>
2023-03-18 23:48:17 -0500

    Added icons and strikethrough capability

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import internal from 'stream';
 import InputField from './components/InputField';
+import { Todo } from "./model"
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => { //FC is a "functional component"
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  //console.log(todo);
-  
+  const handleAdd= (e: React.FormEvent) => {
+    e.preventDefault();  // Added so it doesn't refresh the page everytime
+
+    if(todo) {
+      setTodos([...todos, {id:Date.now(), todo:todo, isDone:false }])
+      setTodo("");
+    }
+  }
+
+  console.log(todos);
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+        ))}
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { updateSourceFile } from 'typescript';
 import './styles.css';
 
 interface Props { // Creating an interface to pass the props through the component
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent)=> void; // Function that is void, should be used onSubmit in the <form>
 }
 
-const InputField = ( { todo, setTodo }: Props) => {
+const InputField = ( { todo, setTodo, handleAdd }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className='input'>
+    <form className='input' onSubmit={(e) => {
+            handleAdd(e);
+            inputRef.current?.blur(); //Question mark becuase it's optional, this also gets rid of blur when you hit 'Enter'
+        }}>
         <input type='input'
+        ref={inputRef}
         value = {todo}
         onChange = {
             (e)=>setTodo(e.target.value)
